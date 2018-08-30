86cf7f433a482743c797fba622adc4eb1d875c50
mario.martinez <mario.martinez@seccionamarilla.com>
2018-08-30 15:35:53 -0500

    v1.02, bugfixed some funny characters like ü, not displaying correctly

--- c.php
+++ c.php
@@ -18,6 +18,12 @@ DEFINE("HELP", "
 	$opts = new Commando\Command();
 
 // Functions
+function fix($i){
+	$t = array(
+    "\u00c0" =>"À",     "\u00c1" =>"Á",     "\u00c2" =>"Â",     "\u00c3" =>"Ã",     "\u00c4" =>"Ä",     "\u00c5" =>"Å",     "\u00c6" =>"Æ",     "\u00c7" =>"Ç",     "\u00c8" =>"È",     "\u00c9" =>"É",     "\u00ca" =>"Ê",     "\u00cb" =>"Ë",     "\u00cc" =>"Ì",     "\u00cd" =>"Í",     "\u00ce" =>"Î",     "\u00cf" =>"Ï",     "\u00d1" =>"Ñ",     "\u00d2" =>"Ò",     "\u00d3" =>"Ó",     "\u00d4" =>"Ô",     "\u00d5" =>"Õ",     "\u00d6" =>"Ö",     "\u00d8" =>"Ø",     "\u00d9" =>"Ù",     "\u00da" =>"Ú",     "\u00db" =>"Û",     "\u00dc" =>"Ü",     "\u00dd" =>"Ý",     "\u00df" =>"ß",     "\u00e0" =>"à",     "\u00e1" =>"á",     "\u00e2" =>"â",     "\u00e3" =>"ã",     "\u00e4" =>"ä",     "\u00e5" =>"å",     "\u00e6" =>"æ",     "\u00e7" =>"ç",     "\u00e8" =>"è",     "\u00e9" =>"é",     "\u00ea" =>"ê",     "\u00eb" =>"ë",     "\u00ec" =>"ì",     "\u00ed" =>"í",     "\u00ee" =>"î",     "\u00ef" =>"ï",     "\u00f0" =>"ð",     "\u00f1" =>"ñ",     "\u00f2" =>"ò",     "\u00f3" =>"ó",     "\u00f4" =>"ô",     "\u00f5" =>"õ",     "\u00f6" =>"ö",     "\u00f8" =>"ø",     "\u00f9" =>"ù",     "\u00fa" =>"ú",     "\u00fb" =>"û",     "\u00fc" =>"ü",     "\u00fd" =>"ý", "\u00ff" =>"ÿ");
+	return strtr($i, $t);
+	}
+
 function arrSearch($hayStack, $needle){
 	$o = array_filter($hayStack, function($el) use ($needle) {
 		return ( stripos($el['name'], $needle) !== false || stripos($el['number'], $needle) !== false );
@@ -38,7 +44,9 @@ function filter($contents){
 	return $exe;
 }
 function showJson($arr){
-	print_r(json_encode($arr));
+	$o = print_r(json_encode($arr, JSON_PRETTY_PRINT),true);
+	$o = fix($o);
+	echo $o;
 }
 
 function show($arr){
@@ -54,7 +62,7 @@ function show($arr){
 	$opts->option('f')->aka('callthefirst')->describedAs('Call the first match')->boolean()->defaultsTo(false);
 	$opts->option('p')->aka('prefix')->describedAs('A dial prefix (optional)')->defaultsTo("");
 	$opts->option('s')->aka('simcall')->describedAs('Simulate call')->boolean()->defaultsTo(false);
-	$opts->option('v')->aka('version')->describedAs('Version 1.01')->boolean()->defaultsTo(false);
+	$opts->option('v')->aka('version')->describedAs('Version 1.02')->boolean()->defaultsTo(false);
 	$opts->option('c')->aka('call')->describedAs("deprecated, this is the default now.")->boolean()->defaultsTo(false);
 	// $opts->option('j')->aka('json')->describedAs('Output in json format')->boolean()->defaultsTo(false);
 
